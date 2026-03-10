c5db303f4bbfee13447ab30169d0228d047deeca
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-03-09 22:19:09 -0300

    feat: constantes

---
+++ constants/banco_dados.py
@@ -0,0 +1,8 @@
+from constants.paths import DATABASE_DIR
+
+
+BANCO_DADOS_CARGAS_PENDENTES = DATABASE_DIR / "cargas_pendentes.db"
+TABELA_CARGAS_PENDENTES = "cargas_pendentes"
+
+BANCO_DADOS_CARGAS_FINALIZADAS = DATABASE_DIR / "cargas_finalizadas.db"
+TABELA_CARGAS_FINALIZADAS = "cargas_finalizadas"
---
+++ constants/textos.py
@@ -0,0 +1,15 @@
+FONTE_TITULO = ("Segoe UI", 22, "bold")
+FONTE_SUBTITULO = ("Segoe UI", 19, "bold")
+
+FONTE_LABEL = ("Segoe UI", 17, "bold")
+
+FONTE_CABECALHO_TREE_VIEW = ("Segoe UI", 16, "bold")
+FONTE_TEXTO_TREE_VIEW = ("Segoe UI", 13)
+
+FONTE_TEXTO = ("Segoe UI", 14)
+FONTE_PEQUENA = ("Segoe UI", 11)
+
+FONTE_BOTAO_PRINCIPAL = ("Segoe UI", 16, "bold")
+FONTE_BOTAO_SECUNDARIO = ("Segoe UI", 14, "bold")
+
+FONTE_LISTBOX = ("Segoe UI", 13)
