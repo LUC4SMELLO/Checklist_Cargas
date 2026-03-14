7a0e543c536a6f3e6c29aa05f80eb7aefd0642e2
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-03-14 17:32:28 -0300

    feat: constante banco dados cargas concluídas

--- constants/banco_dados.py
+++ constants/banco_dados.py
@@ -4,5 +4,5 @@ from constants.paths import DATABASE_DIR
 BANCO_DADOS_CARGAS_PENDENTES = DATABASE_DIR / "cargas_pendentes.db"
 TABELA_CARGAS_PENDENTES = "cargas_pendentes"
 
-BANCO_DADOS_CARGAS_FINALIZADAS = DATABASE_DIR / "cargas_finalizadas.db"
-TABELA_CARGAS_FINALIZADAS = "cargas_finalizadas"
+BANCO_DADOS_CARGAS_CONCLUIDAS = DATABASE_DIR / "cargas_concluidas.db"
+TABELA_CARGAS_CONCLUIDAS = "cargas_concluidas"
