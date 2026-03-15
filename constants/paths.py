4e29476d58ee7f5f179726e1bb183466439dab86
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-03-15 17:45:44 -0300

    feat: melhorando o paths

--- constants/paths.py
+++ constants/paths.py
@@ -1,6 +1,10 @@
+import sys
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+if getattr(sys, "frozen", False):
+    BASE_DIR = Path(sys._MEIPASS)
+else:
+    BASE_DIR = Path(__file__).resolve().parent.parent
 
 ASSETS_DIR = BASE_DIR / "assets"
 ICONS_DIR = ASSETS_DIR / "icons"
