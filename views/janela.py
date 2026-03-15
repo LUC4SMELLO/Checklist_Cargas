4a2a8f3a64875bf7d8c06bfe833f3357ec4861fb
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-03-15 14:38:06 -0300

    feat: adicionando ícone do aplicativo

--- views/janela.py
+++ views/janela.py
@@ -1,10 +1,13 @@
 import tkinter as ttk
 import customtkinter as ctk
+from constants.paths import ICONS_DIR
+
 
 janela = ctk.CTk()
 janela.title("Checklist Cargas")
 janela.geometry("855x540")
 janela.resizable(False, False)
+janela.iconbitmap(ICONS_DIR / "checklist_cargas_icon.ico")
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
