aafb51c73332e437a84fde6186671a363cc98d9f
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-03-15 18:28:19 -0300

    feat: melhoria no ícone do aplicativo

--- views/janela.py
+++ views/janela.py
@@ -1,4 +1,4 @@
-import tkinter as ttk
+import tkinter as tk
 import customtkinter as ctk
 from constants.paths import ICONS_DIR
 
@@ -7,7 +7,9 @@ janela = ctk.CTk()
 janela.title("Checklist Cargas")
 janela.geometry("855x540")
 janela.resizable(False, False)
-janela.iconbitmap(default=str(ICONS_DIR / "checklist_cargas_icon.ico"))
+
+icon = tk.PhotoImage(file=str(ICONS_DIR / "checklist_cargas_icon.png"))
+janela.iconphoto(True, icon)
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
