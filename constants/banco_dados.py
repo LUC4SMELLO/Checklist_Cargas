3f1b3995458dd102300967de5855f769108b3f34
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-03-23 15:24:29 -0300

    feat: adicionando caminho banco dados fixo

--- constants/banco_dados.py
+++ constants/banco_dados.py
@@ -1,5 +1,6 @@
 from constants.paths import DATABASE_DIR
 
 
-BANCO_DADOS_CARGAS = DATABASE_DIR / "cargas.db"
+#BANCO_DADOS_CARGAS = DATABASE_DIR / "cargas.db"
+BANCO_DADOS_CARGAS = r"Z:/cargas.db"
 TABELA_CARGAS = "cargas"
